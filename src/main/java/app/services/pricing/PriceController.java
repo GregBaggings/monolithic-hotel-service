47d9d8e082b4fcdf345b8120a7d7c466ff3ad51b
Gergely_Agnecz <agnegerg@gmail.com>
2017-08-06 13:25:36 +0200

    New endpoint

--- src/main/java/app/services/pricing/PriceController.java
+++ src/main/java/app/services/pricing/PriceController.java
@@ -1,8 +1,12 @@
 package app.services.pricing;
 
+import app.models.Hotel;
+import app.models.HotelsDAO;
 import app.models.Price;
 import app.models.PriceDAO;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,14 +21,36 @@ import java.util.List;
 @RestController
 public class PriceController {
 
+    JSONObject jsonObject = new JSONObject();
     @Autowired
-    PriceDAO dao;
+    PriceDAO priceDAO;
+    @Autowired
+    HotelsDAO hotelDAO;
 
     @RequestMapping("/v1/hotels/price")
     @JsonInclude(JsonInclude.Include.NON_NULL)
-    public ResponseEntity<?> hotelPrices() {
-        List<Price> prices = dao.findAll();
+    public ResponseEntity<?> roomPrices() {
+        List<Price> prices = priceDAO.findAll();
 
         return new ResponseEntity<>(prices, HttpStatus.OK);
+    }
+
+    @RequestMapping("/v1/hotels/prices")
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    public ResponseEntity<?> pricesWithHotel() {
+
+        JSONArray hotelsJSON = new JSONArray();
+        JSONArray pricesJSON = new JSONArray();
+        List<Hotel> hotels = hotelDAO.findAll();
+        List<Price> prices = priceDAO.findAll();
+
+        hotelsJSON.add(hotels);
+        pricesJSON.add(prices);
+
+        jsonObject.put("result", "OK");
+        jsonObject.put("hotels", hotelsJSON);
+        jsonObject.put("roomPrices", pricesJSON);
+
+        return new ResponseEntity<>(jsonObject, HttpStatus.OK);
     }
 }
